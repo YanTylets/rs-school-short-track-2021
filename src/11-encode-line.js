e9b11108aa1ae9838b7fbb786e36145bfeb7b6ee
YanTylets <72673268+YanTylets@users.noreply.github.com>
2021-04-12 04:14:37 +0300

    feat: add solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let newStr = '';
+  let n = 1;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j <= str.length; j++) {
+      if (str[i] === str[j]) {
+        n++;
+        break;
+      } else if (str[i] !== str[j] && n === 1) {
+        newStr += str[i];
+        break;
+      } else {
+        newStr += n + str[i];
+        n = 1;
+        break;
+      }
+    }
+  }
+  return newStr;
 }
 
 module.exports = encodeLine;
